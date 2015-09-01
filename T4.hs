f298b9ee11f210e4eda43b50b6c023b7211be5c2
henrvelho <henrvelho@gmail.com>
2015-08-31 21:53:25 -0300

    update falta exerc 4

--- T4.hs
+++ T4.hs
@@ -9,5 +9,21 @@ eleva (x:xs) = (x^2) : eleva  xs
 contido :: Char -> String -> Bool
 contido _ [] = False
 contido caracter lista = if (head lista == caracter)
-	then True else contido caracter (tail lista)
-	
+then True else contido caracter (tail lista)
+
+--Exercicio 3
+
+semVogais :: String -> String
+semVogais "" = ""
+semVogais lista = if(elem (head lista) "aeiouAEIOU") 
+then semVogais (tail lista) else head lista : semVogais (tail lista)
+
+--Exercicio 4
+
+--
+
+--Exercicio 5
+
+geraTabela' :: Int -> [(Int,Int)]
+geraTabela' 0 = []
+geraTabela' j = geraTabela(j-1) ++ [(j,j^2)]
