42428e0a9bf5bc8d44114803e9b34e802055267f
henrvelho <henrvelho@gmail.com>
2015-08-31 21:32:53 -0300

    Update T4.hs

--- T4.hs
+++ T4.hs
@@ -4,4 +4,10 @@ eleva :: [Int] -> [Int]
 eleva [] = []
 eleva (x:xs) = (x^2) : eleva  xs
 
+--Exercicio 2
 
+contido :: Char -> String -> Bool
+contido _ [] = False
+contido caracter lista = if (head lista == caracter)
+	then True else contido caracter (tail lista)
+	
