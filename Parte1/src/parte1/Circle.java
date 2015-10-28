6a4a3c6f8508de3e908fb3595bc3625c94aa8709
henrvelho <henrvelho@hotmail.com>
2015-10-28 14:53:01 -0200

    Trabalho java

---
+++ Parte1/src/parte1/Circle.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parte1;
+
+import static java.lang.Math.pow;
+
+/**
+ *
+ * @author iffarroupilha09
+ */
+public class Circle {
+    private Ponto ponto;
+    private float raio;
+
+    public Circle(Ponto ponto, float raio) {
+        this.ponto = ponto;
+        this.raio = raio;
+    }
+
+    public Circle(float x, float y, float raio) {
+        Ponto ponto2 = new Ponto(x, y);
+        this.ponto = ponto2;
+        this.raio = raio;
+    }
+    
+    
+
+    public Ponto getPonto() {
+        return ponto;
+    }
+
+    public void setPonto(Ponto ponto) {
+        this.ponto = ponto;
+    }
+
+    public float getRaio() {
+        return raio;
+    }
+
+    public void setRaio(float raio) {
+        this.raio = raio;
+    }
+    
+    public float getDiameter(){
+        return (float) this.raio + this.raio;
+    }
+    
+    public float getArea(){
+        float area = (float) (3.14 * this.raio);
+        area  =  (float) pow(area, 2);
+        return area;
+    }
+    
+    public void imprime(){
+        System.out.println("Ponto(x): " + this.ponto.getX());
+        System.out.println("Ponto(y): " + this.ponto.getY());
+        System.out.println("Raio: " + this.raio);
+    }
+    
+    
+    
+    
+    
+}
---
+++ Parte1/src/parte1/Ponto.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parte1;
+
+import static java.lang.Math.sqrt;
+
+/**
+ *
+ * @author iffarroupilha09
+ */
+public class Ponto {
+    private float x;
+    private float y;
+
+    public Ponto() {
+        this.x = (float) 0.0;
+        this.y = (float) 0.0;
+    }
+
+    public Ponto(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+    
+    
+    
+    public void deslocaPonto(float dx, float dy){
+        this.x = this.x + dx;
+        this.y = this.y + dy;
+    }
+    
+    public float calculaDistancia(float x, float y){
+        float distancia, dx, dy;
+        if(this.x > x)
+            dx = this.x - x;
+        else
+            dx = x - this.x;
+       
+        if(this.y > y)
+            dy = this.y - y;
+        else
+            dy = y - this.y;
+            
+        distancia   = (float) sqrt(dx*dx + dy*dy);
+        return distancia;
+    }
+    
+    
+    
+}
